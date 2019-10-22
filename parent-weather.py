45ee3edfab93e5e5a75544a98be1730d223b8058
Brucepk <brucelee_gui@163.com>
2019-10-22 21:42:09 +0800

    Update parent-weather.py

--- parent-weather.py
+++ parent-weather.py
@@ -1,6 +1,8 @@
 from wxpy import *
 import requests
 from threading import Timer
+from time import sleep
+
 
 """
 本文原创：pk哥
@@ -46,6 +48,7 @@ while True:   # ！！！调试时记得先把while True注释掉，不然会一
     except BaseException:
         my_friend = bot.friends().search(u'xxx')[
             0]  # 发送不成功，则发送消息给自己，提醒消息发送失败 xxx改成你自己微信的昵称
-        my_friend.send(u'天气消息发送失败')
+        my_friend.send(u'天气消息发送失败，请停止程序进行调试')
+        sleep(600)
 
 
